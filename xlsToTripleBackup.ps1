d01c9f336c047a904b5c6ebee87ae3d169a53fdf
matmutant <matmutant@users.noreply.github.com>
2017-03-16 10:57:05 +0100

    removed hardcoded paths

--- xlsToTripleBackup.ps1
+++ xlsToTripleBackup.ps1
@@ -1,23 +1,29 @@
 #The aim of this PowerShell script is to copy a timestamped file (in name) to a directory where only the 3 most recent copies will be kept (sorted by timestamped name using alpha order)
 #The source file should be named BACK_YYYYMMDD_HHmm.xls to make sure the most recent files are at the end of the generated array
-#The script logs everything in log.txt
-Add-Content .\log.txt "`r`n`r`n`r`n$(Get-Date) - Started Copying and cleaning processs"
 #Make sure there is ONLY the file of interest in the Source or you'll get in trouble.
-#Sets the source filename:
-$source = Get-ChildItem ".\data_Source" -Name -Filter *.xls
+#Sets the source directory:
+#Note: don't put a trailing \ at the end of the path
+$sdir = ".\data_Source"
+#Sets the target directory
+#Note: don't put a trailing \ at the end of the path
+$tdir = ".\data_Target"
+#Source filename is set by scanning the folder for the .xls file:
+$source = Get-ChildItem "$sdir" -Name -Filter *.xls
+#The script logs everything in log.txt
+Add-Content $tdir\log.txt "`r`n`r`n`r`n$(Get-Date) - Started Copying and Cleaning processs"
 #checks if there is only one single file listed.
 if ($source.Count -ne 1) {
 	"Aborted, there is not only one file of interest in there."
-	Add-Content .\log.txt "$(Get-Date) - Copy aborted, reason: 0 or more than 1 file in source."
+	Add-Content $tdir\log.txt "$(Get-Date) - Copy aborted, reason: 0 or more than 1 file in source."
 }
 else {
-	#Copies the source *.xls from source directory to .\data
+	#Copies the source *.xls from source directory to target directory
 	"Copying process:"
 	">>> Copying $source file to folder"
-	Copy-Item .\data_Source\$source .\data\
-	Add-Content .\log.txt "$(Get-Date) - Copied $source from source"
+	Copy-Item $sdir\$source $tdir\
+	Add-Content $tdir\log.txt "$(Get-Date) - Copied $source from source directory"
 	#Starts the cleaning process
-	$a = Get-ChildItem ".\data" -Name -Filter *.xls
+	$a = Get-ChildItem "$tdir" -Name -Filter *.xls
 	"Deletion process:"
 	#$a.Count -ge 4 ==> checks if there are at least 3 files in the directory
 	if ($a.Count -ge 4) {
@@ -27,12 +33,12 @@ else {
 		$b = ($a.Count-4)
 		Foreach ($filexls in $a[0..$b]) {
 			">>> deleting: $filexls"
-			Add-Content .\log.txt "$(Get-Date) - Deleted: $filexls"
-			Remove-Item .\data\$filexls
+			Add-Content $tdir\log.txt "$(Get-Date) - Deleted $filexls from target directory"
+			Remove-Item $tdir\$filexls
 		}
 	}
 	else {
 		">>> Deletion aborted, there is at most 3 files in the directory. Keep Yo Backup!"
-		Add-Content .\log.txt "$(Get-Date) - Deletion aborted, reason: not enough backup files"
+		Add-Content $tdir\log.txt "$(Get-Date) - Deletion aborted, reason: not enough backup files"
 	}
 }
