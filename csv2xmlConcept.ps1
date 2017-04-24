8cc36154cd368d75f228c4fd29cf8c5ff5d4060d
matmutant <matmutant@users.noreply.github.com>
2017-04-24 14:42:35 +0200

    Create csv2xmlConcept.ps1

---
+++ csv2xmlConcept.ps1
@@ -0,0 +1,32 @@
+#one line csv to xml converter proof of concept, that features lots of un-assigned issues and would be quite hard to maintain
+#regex matches:
+#matches 4 non ';' char string
+$C4 = '([^;]{4}?)'
+#matches 6 numbers
+$N6 = '([0-9]{6}?)'
+#matches 0 or 35 non ';' char, non-mandatory content
+$Cf35 = '([^;]{35}?)??'
+#matches 0 or 14 non ';' char, non-mandatory content
+$Cf14 = '([^;]{14}?)??'
+#matches 1 non ';' char
+$C1 = '([^;])'
+#matches 13 non ';' char
+$C13 = '([^;]{13}?)'
+#separator:
+$s = ';'
+#hashtable that contains the csv content
+$content = @{
+c0='ENR1;012345;abcdefghijklmnopqrstuvwyxzAZERTYU35;ABCDEFGHIJKLMNOPQRSTUVWYXZazertyu35;0123456789014C;C;azertyuiop13C;012345;ABCDEFGHIJKLMNOPQRSTUVWYXZazertyu35;khkgixchfghdifO0987';
+c1='ENR1;012345;;ABCDEFGHIJKLMNOPQRSTUVWYXZazertyu35;;C;azertyuiop13C;012345;;khkgixchfghdifO0987';
+c2='ENR1;012345;;;;C;azertyuiop13C;012345;;khkgixchfghdifO0987'}
+$template = '<top><First>$1</First></top><content><second>$2</second><code3>$3</code3><codeFour>$4</codeFour><fiVe>$5</fiVe><typeAct>$6</typeAct><Sev>$7</Sev><nfour>$8</nfour><coden>$9</coden><reliq:$10/></content>'
+foreach ($csv in $content.values.getEnumerator()) {
+"`ninput: `n$csv`n"
+"Full regex : `n`'^$C4$s$N6$s$Cf35$s$Cf35$s$Cf14$s$C1$s$C13$s$N6$s$Cf35$s(.*)`'`n"
+"result:"
+$workVar = [regex]::Replace($csv, "^$C4$s$N6$s$Cf35$s$Cf35$s$Cf14$s$C1$s$C13$s$N6$s$Cf35$s(.*)", $template)
+#removes empty tags
+$workVar = $workVar -replace '><[a-zA-Z0-9]*></[a-zA-Z0-9]*><', '><'
+#adds line breaks
+$workVar -replace '><',">`r`n<"
+}
