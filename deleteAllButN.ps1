c6b70023e83624d8ac4fcda4c1955b59df9317bf
matmutant <matmutant@users.noreply.github.com>
2017-04-24 15:25:31 +0200

    revamped variables

--- deleteAllButN.ps1
+++ deleteAllButN.ps1
@@ -1,6 +1,6 @@
 #LICENSE: Creative Commons CC-BY-SA.
 #Script Written by matmutant
-#The aim of this PowerShell script is to remove file(s) from a directory where only the $nkeep most recent copies will be kept (sorted by timestamped name using alpha order)
+#The aim of this PowerShell script is to remove file(s) from a directory where only the $nbFileToKeep most recent copies will be kept (sorted by timestamped name using alpha order)
 #The source file should be named BACK_YYYYMMDD_HHmm.foo to make sure the most recent files are at the end of the generated array
 #Make sure there is ONLY the file of interest in the Source or you'll get in trouble.
 #Set the source and target directories and file extension using the parameters:
@@ -19,6 +19,7 @@ if (($iniFile -like "") -or ($ifExist -eq $false)) {
 	Start-Sleep -s 10
 	exit
 }
+#grabs ini content and push it to hash table
 Get-Content $iniFile |
 foreach-object -begin {$hashIni=@{}} -process {
 	$k = [regex]::split($_,'=')
@@ -26,24 +27,25 @@ foreach-object -begin {$hashIni=@{}} -process {
 		$hashIni.Add($k[0], $k[1])
 	} 
 }
-#pushes ini parameters to existing variables (legacy support)
-$tdir = $hashIni.targetDirectory
-$filext = $hashIni.fileExtension
-$nkeep = $hashIni.nbFileToKeep
-$logfile = $hashIni.logFile
+#extracts hashtable content as many simple variables
+foreach ($var in $hashIni.keys.GetEnumerator()) {
+	$var
+	New-Variable -Name "$var" -Value $hashIni.$var
+	Get-Variable -Name "$var" -ValueOnly
+}
 #displays parameters for debugging 
 "parameters passed to script:"
-">>> target dir: $tdir"
-">>> log filename: $logfile"
-">>> file extension: $filext"
-">>> nb of file(s) to keep in target: $nkeep`n"
-if (($tdir -like "") -or ($filext -like "") -or ($logfile -like "") -or ($nkeep -lt 1)) {
+">>> target dir: $targetDirectory"
+">>> log filename: $logFile"
+">>> file extension: $fileExtension"
+">>> nb of file(s) to keep in target: $nbFileToKeep`n"
+if (($targetDirectory -like "") -or ($fileExtension -like "") -or ($logFile -like "") -or ($nbFileToKeep -lt 1)) {
 	"`n`r`n`r`nMISSING or INVALID parameters in $iniFile"
 	"Use parameter for source, target, file extension and nb of file(s) to keep as following:"
 	'[workDirectory]'
-	'targetDirectory=.\data_Target'
+	'targetargetDirectoryectory=.\data_Target'
 	'[logFileName]'
-	'logFile=logfile.log'
+	'logFile=logFile.log'
 	'[fileExtension]'
 	'fileExtension=csv'
 	'[nbFileToKeep]'
@@ -51,28 +53,30 @@ if (($tdir -like "") -or ($filext -like "") -or ($logfile -like "") -or ($nkeep 
 	Start-Sleep -s 10
 	exit
 }
-#The script logs everything in $logfile
-Add-Content $tdir\$logfile "`r`n`r`n`r`n$(Get-Date) - Started Cleaning processs"
+#The script logs everything in $logFile
+Add-Content $targetDirectory\$logFile "`r`n`r`n`r`n$(Get-Date) - Started Cleaning processs"
 #Starts the cleaning process
-$a = Get-ChildItem "$tdir" -Name -Filter *.$filext
+$a = Get-ChildItem "$targetDirectory" -Name -Filter *.$fileExtension
 "Deletion process:"
 #note that arrays start at 0...
-$n = $nkeep + 1
-#$a.Count -ge $n ==> checks if there are at least $nkeep files in the directory
+$a.count
+$n = [int]$nbFileToKeep + 1
+$n
+#$a.Count -ge $n ==> checks if there are at least $nbFileToKeep files in the directory
 if ($a.Count -ge $n) {
-	">>> This will delete every single *.$filext present in the directory but the $nkeep latest ones, sorted by name."
+	">>> This will delete every single *.$fileExtension present in the directory but the $nbFileToKeep latest ones, sorted by name."
 	">>> Files to be considered: $a"
-	#$b is the array size minus $n, to keep the $nkeep last items
+	#$b is the array size minus $n, to keep the $nbFileToKeep last items
 	$b = ($a.Count-$n)
 	Foreach ($filebak in $a[0..$b]) {
 		">>> deleting: $filebak"
-		Add-Content $tdir\$logfile "$(Get-Date) - Deleted $filebak from target directory, kept $nkeep backup(s)"
-		Remove-Item $tdir\$filebak
+		Add-Content $targetDirectory\$logFile "$(Get-Date) - Deleted $filebak from target directory, kept $nbFileToKeep backup(s)"
+		Remove-Item $targetDirectory\$filebak
 	}
 }
 else {
-	">>> Deletion aborted, there is at most $nkeep files in the directory. Keep Yo Backup!"
-	Add-Content $tdir\$logfile "$(Get-Date) - Deletion aborted, reason: not enough backup files"
+	">>> Deletion aborted, there is at most $nbFileToKeep files in the directory. Keep Yo Backup!"
+	Add-Content $targetDirectory\$logFile "$(Get-Date) - Deletion aborted, reason: not enough backup files"
 }
 #enable sleep for debugging purpose
 Start-Sleep -s 10
