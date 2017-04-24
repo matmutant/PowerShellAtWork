e07156afe3fc6c97deb277487d4292ed0a7cbb68
matmutant <matmutant@users.noreply.github.com>
2017-04-24 15:24:42 +0200

    revamped variables

--- fileToMultipleBackup.ps1
+++ fileToMultipleBackup.ps1
@@ -1,6 +1,6 @@
 #LICENSE: Creative Commons CC-BY-SA.
 #Script Written by matmutant
-#The aim of this PowerShell script is to copy a timestamped file (in name) to a directory where only the $nkeep most recent copies will be kept (sorted by timestamped name using alpha order)
+#The aim of this PowerShell script is to copy a timestamped file (in name) to a directory where only the $nbFileToKeep most recent copies will be kept (sorted by timestamped name using alpha order)
 #The source file should be named BACK_YYYYMMDD_HHmm.foo to make sure the most recent files are at the end of the generated array
 #Make sure there is ONLY the file of interest in the Source or you'll get in trouble.
 #Set the source and target directories and file extension using the parameters:
@@ -19,6 +19,7 @@ if (($iniFile -like "") -or ($ifExist -eq $false)) {
 	Start-Sleep -s 10
 	exit
 }
+#grabs ini content and push it to hash table
 Get-Content $iniFile |
 foreach-object -begin {$hashIni=@{}} -process {
 	$k = [regex]::split($_,'=')
@@ -26,27 +27,27 @@ foreach-object -begin {$hashIni=@{}} -process {
 		$hashIni.Add($k[0], $k[1])
 	} 
 }
-#pushes ini parameters to existing variables (legacy support)
-$sdir = $hashIni.sourceDirectory
-$tdir = $hashIni.targetDirectory
-$filext = $hashIni.fileExtension
-$logfile = $hashIni.logFile
-$nkeep = $hashIni.nbFileToKeep
+#extracts hashtable content as many simple variables
+foreach ($var in $hashIni.keys.GetEnumerator()) {
+	$var
+	New-Variable -Name "$var" -Value $hashIni.$var
+	Get-Variable -Name "$var" -ValueOnly
+}
 #displays parameters for debugging 
 "parameters passed to script:"
-">>> source dir: $sdir"
-">>> target dir: $tdir"
-">>> log filename: $logfile"
-">>> file extension: $filext"
-">>> nb of file(s) to keep in target: $nkeep`n"
-if (($sdir -like "") -or ($tdir -like "") -or ($filext -like "") -or ($logfile -like "") -or ($nkeep -lt 1)) {
+">>> source dir: $sourceDirectory"
+">>> target dir: $targetDirectory"
+">>> log filename: $logFile"
+">>> file extension: $fileExtension"
+">>> nb of file(s) to keep in target: $nbFileToKeep`n"
+if (($sourceDirectory -like "") -or ($targetDirectory -like "") -or ($fileExtension -like "") -or ($logFile -like "") -or ($nbFileToKeep -lt 1)) {
 	"`n`r`n`r`nMISSING or INVALID parameters in $iniFile"
 	"Use parameter for source, target, file extension and nb of file(s) to keep as following:"
 	'[workDirectories]'
 	'sourceDirectory=.\data_Source'
-	'targetDirectory=.\data_Target'
+	'targetargetDirectoryectory=.\data_Target'
 	'[logFileName]'
-	'logFile=logfile.log'
+	'logFile=logFile.log'
 	'[fileExtension]'
 	'fileExtension=csv'
 	'[nbFileToKeep]'
@@ -54,41 +55,41 @@ if (($sdir -like "") -or ($tdir -like "") -or ($filext -like "") -or ($logfile -
 	Start-Sleep -s 10
 	exit
 }
-#Source filename is set by scanning the folder for the .$filext file:
-$source = Get-ChildItem "$sdir" -Name -Filter *.$filext
-#The script logs everything in $logfile
-Add-Content $tdir\$logfile "`r`n`r`n`r`n$(Get-Date) - Started Copying and Cleaning processs"
+#Source filename is set by scanning the folder for the .$fileExtension file:
+$source = Get-ChildItem "$sourceDirectory" -Name -Filter *.$fileExtension
+#The script logs everything in $logFile
+Add-Content $targetDirectory\$logFile "`r`n`r`n`r`n$(Get-Date) - Started Copying and Cleaning processs"
 #checks if there is only one single file listed.
 if ($source.Count -ne 1) {
 	"Aborted, there is not only one file of interest in there."
-	Add-Content $tdir\$logfile "$(Get-Date) - Copy aborted, reason: 0 or more than 1 file in source."
+	Add-Content $targetDirectory\$logFile "$(Get-Date) - Copy aborted, reason: 0 or more than 1 file in source."
 }
 else {
-	#Copies the source *.$filext from source directory to target directory
+	#Copies the source *.$fileExtension from source directory to target directory
 	"Copying process:"
 	">>> Copying $source file to folder"
-	Copy-Item $sdir\$source $tdir\
-	Add-Content $tdir\$logfile "$(Get-Date) - Copied $source from source directory"
+	Copy-Item $sourceDirectory\$source $targetDirectory\
+	Add-Content $targetDirectory\$logFile "$(Get-Date) - Copied $source from source directory"
 	#Starts the cleaning process
-	$a = Get-ChildItem "$tdir" -Name -Filter *.$filext
+	$a = Get-ChildItem "$targetDirectory" -Name -Filter *.$fileExtension
 	"Deletion process:"
 	#note that arrays start at 0...
-	$n = $nkeep + 1
-	#$a.Count -ge $n ==> checks if there are at least $nkeep files in the directory
+	$n = [int]$nbFileToKeep + 1
+	#$a.Count -ge $n ==> checks if there are at least $nbFileToKeep files in the directory
 	if ($a.Count -ge $n) {
-		">>> This will delete every single *.$filext present in the directory but the $nkeep latest ones, sorted by name."
+		">>> This will delete every single *.$fileExtension present in the directory but the $nbFileToKeep latest ones, sorted by name."
 		">>> Files to be considered: $a"
-		#$b is the array size minus $n, to keep the $nkeep last items
+		#$b is the array size minus $n, to keep the $nbFileToKeep last items
 		$b = ($a.Count-$n)
 		Foreach ($filebak in $a[0..$b]) {
 			">>> deleting: $filebak"
-			Add-Content $tdir\$logfile "$(Get-Date) - Deleted $filebak from target directory, kept $nkeep backup(s)"
-			Remove-Item $tdir\$filebak
+			Add-Content $targetDirectory\$logFile "$(Get-Date) - Deleted $filebak from target directory, kept $nbFileToKeep backup(s)"
+			Remove-Item $targetDirectory\$filebak
 		}
 	}
 	else {
-		">>> Deletion aborted, there is at most $nkeep files in the directory. Keep Yo Backup!"
-		Add-Content $tdir\$logfile "$(Get-Date) - Deletion aborted, reason: not enough backup files"
+		">>> Deletion aborted, there is at most $nbFileToKeep files in the directory. Keep Yo Backup!"
+		Add-Content $targetDirectory\$logFile "$(Get-Date) - Deletion aborted, reason: not enough backup files"
 	}
 }
 #enable sleep for debugging purpose
