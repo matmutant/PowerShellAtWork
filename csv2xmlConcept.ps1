83a2460eff2c224e85a964614afc14cce9ff192c
matmutant <matmutant@users.noreply.github.com>
2017-04-25 08:34:47 +0200

    detailed use case for later needs

--- csv2xmlConcept.ps1
+++ csv2xmlConcept.ps1
@@ -1,4 +1,6 @@
 #one line csv to xml converter proof of concept, that features lots of un-assigned issues and would be quite hard to maintain
+#this is based on a use case where the csv content has fixed strings and numbers size in each member of the list like the following example: 
+#4char;6num;0or35char;0or35char;0or14char;1char;13char;6num;0or35char;....
 #regex matches:
 #matches 4 non ';' char string
 $C4 = '([^;]{4}?)'
@@ -16,17 +18,17 @@ $C13 = '([^;]{13}?)'
 $s = ';'
 #hashtable that contains the csv content
 $content = @{
-c0='ENR1;012345;abcdefghijklmnopqrstuvwyxzAZERTYU35;ABCDEFGHIJKLMNOPQRSTUVWYXZazertyu35;0123456789014C;C;azertyuiop13C;012345;ABCDEFGHIJKLMNOPQRSTUVWYXZazertyu35;khkgixchfghdifO0987';
-c1='ENR1;012345;;ABCDEFGHIJKLMNOPQRSTUVWYXZazertyu35;;C;azertyuiop13C;012345;;khkgixchfghdifO0987';
-c2='ENR1;012345;;;;C;azertyuiop13C;012345;;khkgixchfghdifO0987'}
+	c0='ENR1;012345;abcdefghijklmnopqrstuvwyxzAZERTYU35;ABCDEFGHIJKLMNOPQRSTUVWYXZazertyu35;0123456789014C;C;azertyuiop13C;012345;ABCDEFGHIJKLMNOPQRSTUVWYXZazertyu35;khkgixchfghdifO0987';
+	c1='ENR1;012345;;ABCDEFGHIJKLMNOPQRSTUVWYXZazertyu35;;C;azertyuiop13C;012345;;khkgixchfghdifO0987';
+	c2='ENR1;012345;;;;C;azertyuiop13C;012345;;khkgixchfghdifO0987'}
 $template = '<top><First>$1</First></top><content><second>$2</second><code3>$3</code3><codeFour>$4</codeFour><fiVe>$5</fiVe><typeAct>$6</typeAct><Sev>$7</Sev><nfour>$8</nfour><coden>$9</coden><reliq:$10/></content>'
 foreach ($csv in $content.values.getEnumerator()) {
-"`ninput: `n$csv`n"
-"Full regex : `n`'^$C4$s$N6$s$Cf35$s$Cf35$s$Cf14$s$C1$s$C13$s$N6$s$Cf35$s(.*)`'`n"
-"result:"
-$workVar = [regex]::Replace($csv, "^$C4$s$N6$s$Cf35$s$Cf35$s$Cf14$s$C1$s$C13$s$N6$s$Cf35$s(.*)", $template)
-#removes empty tags
-$workVar = $workVar -replace '><[a-zA-Z0-9]*></[a-zA-Z0-9]*><', '><'
-#adds line breaks
-$workVar -replace '><',">`r`n<"
+	"`ninput: `n$csv`n"
+	"Full regex : `n`'^$C4$s$N6$s$Cf35$s$Cf35$s$Cf14$s$C1$s$C13$s$N6$s$Cf35$s(.*)`'`n"
+	"result:"
+	$workVar = [regex]::Replace($csv, "^$C4$s$N6$s$Cf35$s$Cf35$s$Cf14$s$C1$s$C13$s$N6$s$Cf35$s(.*)", $template)
+	#removes empty tags
+	$workVar = $workVar -replace '><[a-zA-Z0-9]*></[a-zA-Z0-9]*><', '><'
+	#adds line breaks
+	$workVar -replace '><',">`r`n<"
 }
